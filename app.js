5ff6a59ddfbdda21b2bb5005eec17ccccb417496
ikramhasib007 <ikramhasib007@gmail.com>
2018-01-22 17:49:15 +0600

    Nodejs Fundamental video 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+console.log('Staring app.');
+
+const fs = require('fs');
+const os = require('os');
+var user = os.userInfo();
+
+fs.appendFile('greeting.txt', `Hello ${user.username}!`, function(err){
+	if(err)
+		console.log('Unable to write file');
+});
+
+//console.log(user);
